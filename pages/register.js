db6969cbf281f3b9d5fcb53871f6ace4e075eab7
owusuvictorjnr <owusuvictor80@gmail.com>
2023-01-14 11:16:52 +0000

    Featured: Register Page

---
+++ pages/register.js
@@ -0,0 +1,15 @@
+import Head from 'next/head';
+import React from 'react';
+
+const Register = () => {
+  return (
+    <div>
+      <Head>
+        <title>Register</title>
+      </Head>
+      <h1>Register</h1>
+    </div>
+  );
+};
+
+export default Register;
