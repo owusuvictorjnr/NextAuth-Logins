461d1432a9e57b62f1d1c9d7a7b68e9ab6273fb8
owusuvictorjnr <owusuvictor80@gmail.com>
2023-01-14 21:05:29 +0000

    Featured: Login

--- pages/login.js
+++ pages/login.js
@@ -8,7 +8,16 @@ const Login = () => {
       <Head>
         <title>Login</title>
       </Head>
-      <h1>Login</h1>
+      <section className="w-3/4 mx-auto flex flex-col gap-10">
+        <div className="title">
+          <h1 className="text-gray-800 text-4xl font-bold py-4">Explore</h1>
+
+          <p className='w-3/4 mx-auto text-gray-400'>
+            loremIrure id fugiat ipsum nostrud nulla ullamco id anim dolore
+            veniam enim.
+          </p>
+        </div>
+      </section>
     </Layout>
   );
 };
