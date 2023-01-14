d40ab7ac5c79be787ff532ec637f4c4416728c45
owusuvictorjnr <owusuvictor80@gmail.com>
2023-01-14 11:16:36 +0000

    Featured: Login Page

---
+++ pages/login.js
@@ -0,0 +1,16 @@
+import Head from 'next/head';
+import React from 'react';
+import Layout from '../components/Layout';
+
+const Login = () => {
+  return (
+    <Layout>
+      <Head>
+        <title>Login</title>
+      </Head>
+      <h1>Login</h1>
+    </Layout>
+  );
+};
+
+export default Login;
