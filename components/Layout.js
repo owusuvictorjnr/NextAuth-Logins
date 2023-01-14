874485c942bc45e7cf66e9f323afaa5da7b758cd
owusuvictorjnr <owusuvictor80@gmail.com>
2023-01-14 11:15:13 +0000

    Featured: Layout for components

---
+++ components/Layout.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Layout = ({ children }) => {
+  return (
+    <div className="flex h-screen bg-blue-400">
+      <div className="m-auto bg-slate-50 rounded-md w-3/5 h-3/4 grid lg:grid-cols-2">
+        <div>images</div>
+
+        <div className="right flex flex-col justify-evenly bg-red-300">
+          <div className="text-center py-10">{children}</div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Layout;
