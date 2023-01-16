658fd72a1d8bb1957ee38748457345118f546a1a
owusuvictorjnr <owusuvictor80@gmail.com>
2023-01-16 23:02:24 +0000

    Featured: Authenticating gmail

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,10 @@
 import Head from 'next/head';
 import Link from 'next/link';
 import { useState } from 'react';
+import { useSession } from 'next-auth/react';
 
 const Home = () => {
-  const [session, setSession] = useState(true);
+  const { data: session } = useSession();
 
   return (
     <div className="container">
@@ -11,7 +12,7 @@ const Home = () => {
         <title>Home Page</title>
       </Head>
 
-      {session ? User() : Guest()}
+      {session ? User({ session }) : Guest()}
     </div>
   );
 };
@@ -35,14 +36,14 @@ const Guest = () => {
 };
 
 // Authorize Use
-const User = () => {
+const User = ({ session }) => {
   return (
     <main className="container mx-auto text-center py-20">
       <h3 className="text-4xl font-bold capitalize">authorize use homepage </h3>
 
       <div className="details capitalize">
-        <h5 className="">unknown</h5>
-        <h5 className="">unknown</h5>
+        <h5 className="">{session.user.name}</h5>
+        <h5 className="">{session.user.email}</h5>
       </div>
 
       <div className="flex justify-center">
